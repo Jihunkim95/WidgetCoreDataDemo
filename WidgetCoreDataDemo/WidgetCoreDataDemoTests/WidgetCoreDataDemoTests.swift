dc165f17be5be7d5a826b15d877b5724c5d130f0
Jihunkim95 <darak551@naver.com>
2023-12-28 15:09:39 +0900

    add: Widget생성 및 연동
    해당 영상 참고
    https://www.youtube.com/watch?v=FV_3kiRF90g

--- WidgetCoreDataDemo/WidgetCoreDataDemoTests/WidgetCoreDataDemoTests.swift
+++ WidgetCoreDataDemo/WidgetCoreDataDemoTests/WidgetCoreDataDemoTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import WidgetCoreDataDemo
 
 final class WidgetCoreDataDemoTests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class WidgetCoreDataDemoTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
